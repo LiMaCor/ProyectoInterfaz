16f2e2747e7a69e702fb46da5928114e0d89075a
PixelZer0 <pixel_zero@hotmail.com>
2017-10-24 00:32:04 +0200

    Check connection at first added

--- proyectoInterface/src/main/java/Servlet/json.java
+++ proyectoInterface/src/main/java/Servlet/json.java
@@ -1,6 +1,8 @@
 package Servlet;
 
 import Beans.ReplyBean;
+import Connection.ConnectionInterface;
+import Helper.AppConfigurationHelper;
 import Helper.EstadoHelper;
 import Helper.EstadoHelper.Tipo_estado;
 import static Helper.ParameterCook.prepareCamelCaseObject;
@@ -9,6 +11,10 @@ import Static.Log4jStatic;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.lang.reflect.Method;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -39,19 +45,10 @@ public class json extends HttpServlet {
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json;charset=UTF-8");
+            throws ServletException, IOException, SQLException, Exception {
+        //response.setContentType("application/json;charset=UTF-8");
         ReplyBean oReplyBean = null;
         try (PrintWriter out = response.getWriter()) {
-
-            response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
-            //response.setHeader("Access-Control-Allow-Methods", "PATCH, POST, GET, PUT, OPTIONS, DELETE");
-            response.setHeader("Access-Control-Allow-Methods", "GET,POST");
-            response.setHeader("Access-Control-Max-Age", "86400");
-            response.setHeader("Access-Control-Allow-Credentials", "true");
-            response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, x-requested-with, Content-Type");
-
-            retardo(0);
 
             try {
                 Class.forName("com.mysql.jdbc.Driver");
@@ -59,28 +56,80 @@ public class json extends HttpServlet {
                 oReplyBean = new ReplyBean(500, "Database Connection Error: Please contact your administrator");
             }
 
+            retardo(0);
+
             String ob = prepareCamelCaseObject(request);
             String op = request.getParameter("op");
-            try {
-                String strClassName = "Services." + ob + "Service";
-                ViewServiceInterface oService = (ViewServiceInterface) Class.forName(strClassName).getDeclaredConstructor(HttpServletRequest.class).newInstance(request);
-                Method oMethodService = oService.getClass().getMethod(op);
-                oReplyBean = (ReplyBean) oMethodService.invoke(oService);
-            } catch (Exception ex) {
-                if (EstadoHelper.getTipo_estado() == Tipo_estado.Debug) {
-                    out.println(ex);
-                    ex.printStackTrace(out);
-                } else {
-                    oReplyBean = new ReplyBean(500, "carrito-server error. Please, contact your administrator.");
+
+            if (("".equalsIgnoreCase(ob) && "".equalsIgnoreCase(op)) || (ob == null && op == null)) {
+                Connection oConnection = null;
+                ConnectionInterface oPooledConnection = null;
+                try {
+                    oPooledConnection = AppConfigurationHelper.getSourceConnection();
+                    oConnection = oPooledConnection.newConnection();
+                    response.setContentType("text/html;charset=UTF-8");
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Carrito server</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.print("<h1>Bienvenidos a carrito-server</h1>");
+                    out.println("<h2>Servlet json at " + request.getContextPath() + "</h2>");
+                    out.print("<h3>Conexión OK</h3>");
+                    out.println("</body>");
+                    out.println("</html>");
+                } catch (Exception ex) {
+                    response.setContentType("text/html;charset=UTF-8");
+                    out.println("<!DOCTYPE html>");
+                    out.println("<html>");
+                    out.println("<head>");
+                    out.println("<title>Carrito server</title>");
+                    out.println("</head>");
+                    out.println("<body>");
+                    out.print("<h1>Bienvenidos a carrito-server</h1>");
+                    out.println("<h2>Servlet json at " + request.getContextPath() + "</h2>");
+                    out.print("<h3>Conexión KO</h3>");
+                    out.println("</body>");
+                    out.println("</html>");
+                } finally {
+                    if (oConnection != null) {
+                        oConnection.close();
+                    }
+                    if (oPooledConnection != null) {
+                        oPooledConnection.disposeConnection();
+                    }
                 }
-                Log4jStatic.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+            } else {
+                response.setHeader("Access-Control-Allow-Origin", request.getHeader("origin"));
+                //response.setHeader("Access-Control-Allow-Methods", "PATCH, POST, GET, PUT, OPTIONS, DELETE");
+                response.setHeader("Access-Control-Allow-Methods", "GET,POST");
+                response.setHeader("Access-Control-Max-Age", "86400");
+                response.setHeader("Access-Control-Allow-Credentials", "true");
+                response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, x-requested-with, Content-Type");
+
+                try {
+                    String strClassName = "Services." + ob + "Service";
+                    ViewServiceInterface oService = (ViewServiceInterface) Class.forName(strClassName).getDeclaredConstructor(HttpServletRequest.class).newInstance(request);
+                    Method oMethodService = oService.getClass().getMethod(op);
+                    oReplyBean = (ReplyBean) oMethodService.invoke(oService);
+                } catch (Exception ex) {
+                    if (EstadoHelper.getTipo_estado() == Tipo_estado.Debug) {
+                        out.println(ex);
+                        ex.printStackTrace(out);
+                    } else {
+                        oReplyBean = new ReplyBean(500, "carrito-server error. Please, contact your administrator.");
+                    }
+                    Log4jStatic.errorLog(this.getClass().getName() + ":" + (ex.getStackTrace()[0]).getMethodName(), ex);
+                }
+                out.print("{\"status\":" + oReplyBean.getCode() + ", \"json\":" + oReplyBean.getJson() + "}");
             }
-            out.print("{\"status\":" + oReplyBean.getCode() + ", \"json\":" + oReplyBean.getJson() + "}");
+
         }
 
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -92,7 +141,11 @@ public class json extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(json.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -106,7 +159,11 @@ public class json extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        try {
+            processRequest(request, response);
+        } catch (Exception ex) {
+            Logger.getLogger(json.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
